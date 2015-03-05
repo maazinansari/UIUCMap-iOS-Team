e0aa6b1a8cd80ae17443e15785a90fff0db02377
mansari2 <mansari2@illinois.edu>
2015-03-04 18:53:24 -0600

    2

---
+++ First MapTests/First_MapTests.swift
@@ -0,0 +1,36 @@
+//
+//  First_MapTests.swift
+//  First MapTests
+//
+//  Created by Maazin Ansari on 3/2/15.
+//  Copyright (c) 2015 Maazin Ansari. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class First_MapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
