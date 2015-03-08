41ac18ee93243291f4b4117754acaba2b673d383
mansari2 <mansari2@illinois.edu>
2015-03-08 18:00:59 -0500

    Testing Google Maps

---
+++ GMapTest/GMapTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GMapTest
+//
+//  Created by Maazin Ansari on 3/8/15.
+//  Copyright (c) 2015 Maazin Ansari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstMapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
